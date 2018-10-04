9a49309bfa757e596c138208397b1190957c827b
Sean Nealon <signalflowsean@gmail.com>
2018-10-04 11:58:02 -0400

    added boilerplate store

---
+++ scripts/store.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// eslint-disable-next-line no-unused-vars
+const STORE = (function(){ 
+  
+  const addItem = function(item) { 
+
+  }; 
+
+  const findById = function(id) { 
+
+  }; 
+
+  const findAndDelete = function(id) { 
+
+  }; 
+
+  return {
+    items: [], 
+    error: null,
+    addItem, 
+    findById, 
+    findAndDelete
+  }; 
+}()); 
