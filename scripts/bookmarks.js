5ec617b8ec2cac2b82f2e39dc551b058d72b3090
Sean Nealon <signalflowsean@gmail.com>
2018-10-04 13:47:36 -0400

    added boilerplate event listeners

---
+++ scripts/bookmarks.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// eslint-disable-next-line no-unused-vars
+const bookmarks = (function(){ 
+  function handleNewItemSubmit(){ 
+
+  }
+
+  function handleDeleteItemClicked(){  
+
+  }
+
+  function handleFilterByRating(){ 
+
+  }
+
+  function bindEventListeners(){ 
+
+  }
+
+  function render(){ 
+
+  }
+
+  return { 
+    render, 
+    bindEventListeners
+  }; 
+
+}()); 
