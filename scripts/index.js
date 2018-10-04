8538d3d949cd52ba6ca03b0916153a202f96a7ee
Sean Nealon <signalflowsean@gmail.com>
2018-10-04 15:04:48 -0400

    items are permenant. AKA: they render after refresh

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,13 @@
 'use strict'; 
-/* global bookmarks */
+/* global api, STORE, bookmarks */
 /*eslint-env jquery*/
 
 $(document).ready(function() { 
   bookmarks.bindEventListeners(); 
   bookmarks.render(); 
+
+  api.getItems((items) => { 
+    items.forEach(item => STORE.addItem(item));
+    bookmarks.render(); 
+  }); 
 }); 
