76e8647f6ea5b150fdd4be8081a1f0a786c56d00
Sean Nealon <signalflowsean@gmail.com>
2018-10-05 14:50:48 -0400

    url entry is wider

---
+++ scripts/jQueryExtend.js
@@ -0,0 +1,11 @@
+'use strict'; 
+/*eslint-env jquery*/
+
+$.fn.extend({
+  serializeJson: function() {
+    const formData = new FormData(this[0]);
+    const o = {};
+    formData.forEach((val, name) => o[name] = val);
+    return JSON.stringify(o);
+  }
+});
